c0caa86ef02ec5bf64ab128c501aec87592463b8
Kallycta <83584576+Kallycta@users.noreply.github.com>
2023-11-27 09:21:50 +0300

    add project

---
+++ main.js
@@ -0,0 +1,1 @@
+const em=document.createElement("script");em.type="text/javascript",em.async=!0,em.src="icallback.js";const s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(em,s);
